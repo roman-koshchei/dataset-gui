fffbd289c48d3d0c9740bc206af3b262b36ac100
Rostyslav Derkach <rostyslavderkach05@gmail.com>
2025-09-21 16:19:42 +0300

    add aria-label attribute

--- src/lib/Tab.svelte
+++ src/lib/Tab.svelte
@@ -72,6 +72,7 @@
                                 class="hover:bg-red-900 rounded p-1 mr-1 transition-opacity duration-150"
                                 onclick={(event) => closeTab(index, event)}
                                 title="Close tab"
+                                aria-label="Close tab"
                         >
                             <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -85,6 +86,7 @@
                     class="flex-shrink-0 px-3 py-2 text-gray-400 hover:text-gray-200 hover:bg-gray-800 transition-colors duration-150"
                     onclick={addNewTab}
                     title="Add new tab"
+                    aria-label="Add new tab"
             >
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
