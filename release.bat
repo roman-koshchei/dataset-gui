388aeca1638a97988f159a2c4220769b3276e8b3
Roman Koshchei <romankoshchei@gmail.com>
2026-04-13 17:15:55 +0300

    release bat

--- release.bat
+++ release.bat
@@ -1,1 +1,7 @@
-.\src-tauri\target\release\dataset-gui.exe
+@echo off
+if not exist ".\src-tauri\target\release\dataset-gui.exe" (
+    echo Release executable not found. Building...
+    call npm install
+    call npm run tauri build
+)
+start "" ".\src-tauri\target\release\dataset-gui.exe"
